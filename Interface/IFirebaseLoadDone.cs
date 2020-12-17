36da12082675e40602f0551c3f47f0d3e27e8af6
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2020-12-17 09:51:19 +0700

    Add project files.

---
+++ Interface/IFirebaseLoadDone.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FirebaseXamarin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirebaseXamarin.Interface
+{
+    public interface IFirebaseLoadDone
+    {
+        void OnFirebaseLoadSuccess(List<ItemMovie> movieList);
+        void OnFirebaseLoadFailed(string message);
+    }
+}
---
+++ Model/ItemMovie.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirebaseXamarin.Model
+{
+    public class ItemMovie
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+    }
+}
